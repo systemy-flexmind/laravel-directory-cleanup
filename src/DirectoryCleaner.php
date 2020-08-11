6daaf3d4ca6ef6bfda8b4695cd51d120d1c52aef
Krzysztof Bielecki <51311340+systemy-flexmind@users.noreply.github.com>
2020-08-11 09:15:06 +0200

    Update DirectoryCleaner.php

--- src/DirectoryCleaner.php
+++ src/DirectoryCleaner.php
@@ -51,7 +51,7 @@ class DirectoryCleaner
 
         $files = collect($this->filesystem->files($workingDir, true))
             ->filter(function ($file) {
-                $lastModifyDate = $this->storage->lastModified($file);
+                $lastModifyDate = $this->filesystem->lastModified($file);
                 return Carbon::createFromTimestamp($lastModifyDate))
                     ->lt($this->timeInPast);
             })
